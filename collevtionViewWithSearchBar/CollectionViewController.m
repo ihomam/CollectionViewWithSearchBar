cc3d7109a290d755b813a2b31b50b8c34656621b
Homam Mawaldi <ihomam.m@gmail.com>
2015-03-06 21:13:19 +0100

    - Fixed Crash: Tapping search and scrolling + support for UIRefreshControl and lil improvements

--- collevtionViewWithSearchBar/CollectionViewController.m
+++ collevtionViewWithSearchBar/CollectionViewController.m
@@ -9,11 +9,15 @@
 #import "CollectionViewController.h"
 #import "CollectionViewCell.h"
 @interface CollectionViewController ()<UISearchBarDelegate>
+
     @property (nonatomic,strong) NSArray        *dataSource;
     @property (nonatomic,strong) NSArray        *dataSourceForSearchResult;
-    @property (nonatomic,strong) UISearchBar    *searchBar;
     @property (nonatomic)        BOOL           searchBarActive;
     @property (nonatomic)        float          searchBarBoundsY;
+
+    @property (nonatomic,strong) UISearchBar        *searchBar;
+    @property (nonatomic,strong) UIRefreshControl   *refreshControl;
+
 @end
 
 @implementation CollectionViewController
@@ -33,16 +37,9 @@ static NSString * const reuseIdentifier = @"Cell";
 }
 
 
-
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
-    
-    // prepare the searchBar view
-    [self prepareSearchBar];
-    
-    // prepare collection view contentInset/ContentOffset so searchBar fit at the top
-    self.collectionView.contentInset    = UIEdgeInsetsMake(self.searchBar.frame.size.height, 5, 0, 5);
-    self.collectionView.contentOffset   = CGPointMake(0, -self.searchBar.frame.size.height);
+    [self prepareUI];
 }
 -(void)viewDidAppear:(BOOL)animated{
     // add KVO observer.. so we will be informed when user scroll colllectionView
@@ -56,36 +53,29 @@ static NSString * const reuseIdentifier = @"Cell";
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-#pragma mark -
--(void)prepareSearchBar{
-    if (!self.searchBar) {
-        self.searchBarBoundsY = self.navigationController.navigationBar.frame.size.height + [UIApplication sharedApplication].statusBarFrame.size.height;
-        self.searchBar = [[UISearchBar alloc]initWithFrame:CGRectMake(0, self.searchBarBoundsY, [UIScreen mainScreen].bounds.size.width, 44)];
-        self.searchBar.searchBarStyle       = UISearchBarStyleMinimal;
-        self.searchBar.showsCancelButton    = YES;
-        self.searchBar.tintColor            = [UIColor whiteColor];
-        self.searchBar.barTintColor         = [UIColor whiteColor];
-        self.searchBar.delegate             = self;
-        self.searchBar.placeholder          = @"search here";
-        
-        [[UITextField appearanceWhenContainedIn:[UISearchBar class], nil] setTextColor:[UIColor whiteColor]];
-        
-        [self.view addSubview:self.searchBar];
-    }
+
+
+#pragma mark - actions
+-(void)refreashControlAction{
+    [self cancelSearching];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // stop refreshing after 2 seconds
+        [self.collectionView reloadData];
+        [self.refreshControl endRefreshing];
+    });
 }
-#pragma mark - CollectionViewDataSourc
+
+
+#pragma mark - <UICollectionViewDataSource>
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
     return 1;
 }
-
-
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
     if (self.searchBarActive) {
         return self.dataSourceForSearchResult.count;
     }
     return self.dataSource.count;
 }
-
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     CollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
     
@@ -99,6 +89,20 @@ static NSString * const reuseIdentifier = @"Cell";
 }
 
 
+#pragma mark -  <UICollectionViewDelegateFlowLayout>
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView
+                        layout:(UICollectionViewLayout*)collectionViewLayout
+        insetForSectionAtIndex:(NSInteger)section{
+    return UIEdgeInsetsMake(self.searchBar.frame.size.height, 0, 0, 0);
+}
+- (CGSize)collectionView:(UICollectionView *)collectionView
+                  layout:(UICollectionViewLayout*)collectionViewLayout
+  sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
+    CGFloat cellLeg = (self.collectionView.frame.size.width/2) - 5;
+    return CGSizeMake(cellLeg,cellLeg);;
+}
+
+
 #pragma mark - search
 - (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope{
     NSPredicate *resultPredicate    = [NSPredicate predicateWithFormat:@"self contains[c] %@", searchText];
@@ -109,23 +113,21 @@ static NSString * const reuseIdentifier = @"Cell";
     // user did type something, check our datasource for text that looks the same
     if (searchText.length>0) {
         // search and reload data source
+        self.searchBarActive = YES;
         [self filterContentForSearchText:searchText
                                    scope:[[self.searchDisplayController.searchBar scopeButtonTitles]
                                           objectAtIndex:[self.searchDisplayController.searchBar
                                                          selectedScopeButtonIndex]]];
         [self.collectionView reloadData];
     }else{
-        // if text lenght == 0 ... means no text
-        // display all content on user face :D
-        self.dataSourceForSearchResult = self.dataSource;
-        [self.collectionView reloadData];
+        // if text lenght == 0
+        // we will consider the searchbar is not active
+        self.searchBarActive = NO;
     }
 }
 
 - (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar{
-    self.searchBarActive = NO;
-    searchBar.text       = @"";
-    [searchBar resignFirstResponder];
+    [self cancelSearching];
     [self.collectionView reloadData];
 }
 - (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar{
@@ -133,11 +135,64 @@ static NSString * const reuseIdentifier = @"Cell";
     [self.view endEditing:YES];
 }
 - (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar{
-    self.searchBarActive = YES;
+    // we used here to set self.searchBarActive = YES
+    // but we'll not do that any more... it made problems
+    // it's better to set self.searchBarActive = YES when user typed something
+    [self.searchBar setShowsCancelButton:YES animated:YES];
 }
 - (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar{
+    // this method is being called when search btn in the keyboard tapped
+    // we set searchBarActive = NO
+    // but no need to reloadCollectionView
     self.searchBarActive = NO;
+    [self.searchBar setShowsCancelButton:NO animated:YES];
 }
+-(void)cancelSearching{
+    self.searchBarActive = NO;
+    [self.searchBar resignFirstResponder];
+    self.searchBar.text  = @"";
+}
+#pragma mark - prepareVC
+-(void)prepareUI{
+    [self addSearchBar];
+    [self addRefreshControl];
+}
+-(void)addSearchBar{
+    if (!self.searchBar) {
+        self.searchBarBoundsY = self.navigationController.navigationBar.frame.size.height + [UIApplication sharedApplication].statusBarFrame.size.height;
+        self.searchBar = [[UISearchBar alloc]initWithFrame:CGRectMake(0,self.searchBarBoundsY, [UIScreen mainScreen].bounds.size.width, 44)];
+        self.searchBar.searchBarStyle       = UISearchBarStyleMinimal;
+        self.searchBar.tintColor            = [UIColor whiteColor];
+        self.searchBar.barTintColor         = [UIColor whiteColor];
+        self.searchBar.delegate             = self;
+        self.searchBar.placeholder          = @"search here";
+        
+        [[UITextField appearanceWhenContainedIn:[UISearchBar class], nil] setTextColor:[UIColor whiteColor]];
+    }
+    
+    if (![self.searchBar isDescendantOfView:self.view]) {
+        [self.view addSubview:self.searchBar];
+    }
+}
+
+-(void)addRefreshControl{
+    if (!self.refreshControl) {
+        self.refreshControl                  = [UIRefreshControl new];
+        self.refreshControl.tintColor        = [UIColor whiteColor];
+        [self.refreshControl addTarget:self
+                                action:@selector(refreashControlAction)
+                      forControlEvents:UIControlEventValueChanged];
+    }
+    if (![self.refreshControl isDescendantOfView:self.collectionView]) {
+        [self.collectionView addSubview:self.refreshControl];
+    }
+}
+-(void)startRefreshControl{
+    if (!self.refreshControl.refreshing) {
+        [self.refreshControl beginRefreshing];
+    }
+}
+
 #pragma mark - observer 
 - (void)addObservers{
     [self.collectionView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:nil];
@@ -147,9 +202,8 @@ static NSString * const reuseIdentifier = @"Cell";
 }
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(UICollectionView *)object change:(NSDictionary *)change context:(void *)context{
     if ([keyPath isEqualToString:@"contentOffset"] && object == self.collectionView ) {
-
         self.searchBar.frame = CGRectMake(self.searchBar.frame.origin.x,
-                                          (-1* object.contentOffset.y)-self.searchBar.frame.size.height,
+                                          self.searchBarBoundsY + ((-1* object.contentOffset.y)-self.searchBarBoundsY),
                                           self.searchBar.frame.size.width,
                                           self.searchBar.frame.size.height);
     }
